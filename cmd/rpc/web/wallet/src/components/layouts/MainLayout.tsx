49cf320210c7a6700394b868f70157f8ea817a16
thanhdat <mstbuon@gmail.com>
2026-06-13 14:41:41 +0700

    Fix RepuRing route remounting

--- cmd/rpc/web/wallet/src/components/layouts/MainLayout.tsx
+++ cmd/rpc/web/wallet/src/components/layouts/MainLayout.tsx
@@ -1,10 +1,8 @@
-import { Outlet, useLocation } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { AppSidebar } from './AppSidebar'
 import { TopBar } from './TopBar'
 
 export default function MainLayout() {
-    const location = useLocation()
-
     return (
         <div className="relative flex h-dvh min-h-dvh overflow-hidden bg-background">
             <AppSidebar />
@@ -14,7 +12,7 @@ export default function MainLayout() {
 
                 <main className="relative z-10 min-h-0 flex-1 overflow-y-auto pt-[4.25rem] lg:pt-0">
                     <div className="mx-auto max-w-[1600px] px-4 py-4 sm:px-5 sm:py-5 lg:py-4">
-                        <Outlet key={location.pathname} />
+                        <Outlet />
                     </div>
                 </main>
             </div>
